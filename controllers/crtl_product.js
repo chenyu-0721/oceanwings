65c6c53634981880591d2bc00819d60fdf7d4921
Guanyu <guanyuchen630602@gmail.com>
2024-12-09 11:39:31 +0800

    邏輯拆成controllers

---
+++ controllers/crtl_product.js
@@ -0,0 +1,65 @@
+const Product = require('../models/product')
+const handleSuccess = require('../handleSuccess.js')
+const handleError = require('../handleError.js')
+
+exports.getProducts = async (req, res, next) => {
+	try {
+		const page = parseInt(req.query.page) || 1
+		const limit = parseInt(req.query.limit) || 10
+
+		const skip = (page - 1) * limit
+
+		const totalItems = await Product.countDocuments()
+
+		const products = await Product.find().skip(skip).limit(limit)
+
+		const totalPages = Math.ceil(totalItems / limit)
+
+		const responseData = {
+			data: products,
+			meta: {
+				totalItems,
+				totalPages,
+				currentPage: page,
+			},
+		}
+
+		handleSuccess(res, responseData)
+	} catch (err) {
+		handleError(res, '取得商品失敗')
+	}
+}
+
+exports.addProduct = async (req, res, next) => {
+	try {
+		const post = await Product.create(req.body)
+		handleSuccess(res, post)
+	} catch (err) {
+		const error = '建立失敗'
+		handleError(res, error)
+	}
+}
+
+exports.editProduct = async (req, res, next) => {
+	try {
+		console.log(req.body)
+
+		const newpost = await Product.findByIdAndUpdate(req.params.id, req.body, {
+			new: true,
+		})
+		handleSuccess(res, newpost)
+	} catch (err) {
+		const error = '編輯失敗'
+		handleError(res, error)
+	}
+}
+
+exports.deleteProduct = async (req, res, next) => {
+	try {
+		const delpost = await Product.findByIdAndDelete(req.params.id)
+		handleSuccess(res, delpost)
+	} catch (err) {
+		const error = '刪除單筆失敗'
+		handleError(error)
+	}
+}
--- routes/product.js
+++ routes/product.js
@@ -1,67 +1,13 @@
 const express = require('express')
 const router = express.Router()
-const Product = require('../models/product.js')
-const handleSuccess = require('../handleSuccess.js')
-const handleError = require('../handleError.js')
+const product_controllers = require('../controllers/crtl_product.js')
 
-router.get('/', async (req, res, next) => {
-	try {
-		const page = parseInt(req.query.page) || 1
-		const limit = parseInt(req.query.limit) || 10
+router.get('/', product_controllers.getProducts)
 
-		const skip = (page - 1) * limit
+router.post('/', product_controllers.addProduct)
 
-		const totalItems = await Product.countDocuments()
+router.patch('/:id', product_controllers.editProduct)
 
-		const products = await Product.find().skip(skip).limit(limit)
-
-		const totalPages = Math.ceil(totalItems / limit)
-
-		const responseData = {
-			data: products,
-			meta: {
-				totalItems,
-				totalPages,
-				currentPage: page,
-			},
-		}
-
-		handleSuccess(res, responseData)
-	} catch (err) {
-		handleError(res, '取得商品失敗')
-	}
-})
-
-router.post('/', async (req, res, next) => {
-	try {
-		const post = await Product.create(req.body)
-		handleSuccess(res, post)
-	} catch (err) {
-		const error = '建立失敗'
-		handleError(res, error)
-	}
-})
-
-router.patch('/:id', async (req, res, next) => {
-	try {
-		const newpost = await Product.findByIdAndUpdate(req.params.id, req.body, {
-			new: true,
-		})
-		handleSuccess(res, newpost)
-	} catch (err) {
-		const error = '編輯失敗'
-		handleError(res, error)
-	}
-})
-
-router.delete('/:id', async (req, res, next) => {
-	try {
-		const delpost = await Product.findByIdAndDelete(req.params.id)
-		handleSuccess(res, delpost)
-	} catch (err) {
-		const error = '刪除單筆失敗'
-		handleError(res, error)
-	}
-})
+router.delete('/:id', product_controllers.deleteProduct)
 
 module.exports = router
